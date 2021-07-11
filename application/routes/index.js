1d5935300cd7eb79704ce2824e66f8b0f824149e
Sarah <sarahsyouf02@gmail.com>
2021-07-11 16:42:59 -0700

    work for milestone 1

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name:"[Insert your name here]" });
+  res.render('index', { title: 'Express', name:"Sarah Abusaif" });
 });
 
 module.exports = router;
