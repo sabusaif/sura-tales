a98cf413da8dcf3ae391961e5df42f078ae07420
Sarah <sarahsyouf02@gmail.com>
2021-07-19 20:26:50 -0700

    completed Milestone 2

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name:"Sarah Abusaif" });
+  res.render('index', { title: 'Sura Tales', name:"Sarah Abusaif" });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Login', name:"Sarah Abusaif" });
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('registration', { title: 'Registration', name:"Sarah Abusaif" });
+});
+
+router.get('/post_image', (req, res, next) => {
+  res.render('postimage', { title: 'Post Image', name:"Sarah Abusaif" });
+});
+
+router.get('/image_post', (req, res, next) => {
+  res.render('imagepost', { title: 'Image Post', name:"Sarah Abusaif" });
 });
 
 module.exports = router;
